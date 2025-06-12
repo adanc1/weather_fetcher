4df674ed977e09b9cac6e94141421fbee6caed5a
adanc1 <adanc@softseveinc.com>
2025-06-12 07:17:37 +0300

    added query params to weather-data endpoint, created schema to return the city as a nested object

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 from datetime import date, datetime, UTC
 
@@ -8,6 +8,11 @@ class Location(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     city: str = Field(max_length=100, nullable=False, unique=True)
+
+    weather_data: Optional["WeatherData"] = Relationship(
+        back_populates="city"
+    )
+
 
 
 class WeatherData(SQLModel, table=True):
@@ -22,6 +27,11 @@ class WeatherData(SQLModel, table=True):
     weather_main: Optional[str] = Field(default=None, max_length=50)
     created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
     created_by: int = Field(default=1)
+
+    city: Optional["Location"] = Relationship(
+        back_populates="weather_data",
+        sa_relationship_kwargs={'lazy': 'selectin'}
+    )
 
 
 class DailyWeatherStats(SQLModel, table=True):
